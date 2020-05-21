391cf2b6e3188782dfabc4d2f304b3f59dfeaaae
Rob Francken <robertfrancken@gmail.com>
2020-05-21 20:56:11 +1000

    Add markup options

--- config/config.php
+++ config/config.php
@@ -1,7 +1,12 @@
 <?php
 
 return [
+    // 'markup' => 'bootstrap-4',
+    // 'markup' => 'bootstrap-3',
+    // 'markup' => 'bulma',
+    // 'markup' => 'foundation',
+    // 'markup' => 'custom',
     // 'segments' => [
-    //     'namespace' => 'App\\Breadcrumbs\\Segments'
+    //     'namespace' => 'App\\Breadcrumbs\\Segments\\'
     // ]
 ];
