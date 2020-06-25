695a89693d708289455884a9285761037ee934de
Rob Francken <robertfrancken@gmail.com>
2020-06-25 23:20:13 +1000

    Fix config typo

--- src/BreadcrumbsServiceProvider.php
+++ src/BreadcrumbsServiceProvider.php
@@ -21,7 +21,7 @@ class BreadcrumbsServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('breadcrumber.php'),
+                __DIR__.'/../config/config.php' => config_path('breadcrumbs.php'),
             ], 'config');
 
             // Publishing the views.
