6d7de8e37d19df5c648f3cad0af2ec1bb08323f7
Rob Francken <robertfrancken@gmail.com>
2020-06-25 21:25:16 +1000

    Add root node config

--- config/config.php
+++ config/config.php
@@ -2,6 +2,18 @@
 
 return [
     /**
+     * Define the breadcrumbs root node here. The icon supports raw HTML, so you are free choose any 
+     * icon markup as long as your app knows how to render it. The icon can be removed by setting
+     * it 'false'. The "You are here:" text can be disabled by setting if false too
+     */
+    'root' => [
+        'label' => 'Home',
+        'url' => env('APP_URL'),
+        'icon' => false,
+        'you_are_here' => false,
+    ],
+
+    /**
      * The template to use for rendering out breadcrumbs. If you want, you can even define your
      * own template. This template should be provided in your views directory under the path
      * resources/views/breadcrumbs/yourBladeTemplate.blade.php
